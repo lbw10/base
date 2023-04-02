42f272103ad1ddeb1f6e5ee79260c056f30dfd86
saqaq <842247799@qq.com>
2023-04-02 16:49:01 +0800

    更新 24.js

--- 24.js
+++ 24.js
@@ -64,7 +64,7 @@ const title = getVal('title') || 'Mega'
 
 const disabled = getVal('disabled')
 /* 混淆 */
-const host = getVal('host') || 'dm.toutiao.com'
+const host = getVal('host') || 'p11.douyinpic.com'
 /* 路径 */
 const pathOpt = getVal('path')
 /* network */
@@ -77,7 +77,7 @@ const prefix = getVal('prefix') || ' '
 const suffix = getVal('suffix') || ''
 /* 附加 Host 前缀 */
 const hostPrefix = getVal('hostPrefix') || 'name'
-const hostSuffix = getVal('hostSuffix') || '|头条|'
+const hostSuffix = getVal('hostSuffix') || '|抖音|'
 /* 附加 Path 前缀 */
 const pathPrefix = getVal('pathPrefix') || ''
 const pathSuffix = getVal('pathSuffix') || ''
