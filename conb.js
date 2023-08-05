9d4b87e61ea0241a70ded3d2ddcfbc3f3ea2aedb
lbw10 <842247799@qq.com>
2023-08-05 15:20:00 +0800

    Update and rename dingvm.js to conb.js

---
+++ conb.js
@@ -0,0 +1,24 @@
+async function operator(proxies = []) {
+  const _ = lodash
+  return proxies.map((p = {}) => {
+    if(_.get(p, 'type') === 'vmess') {
+      //const name = _.get(p, 'name') || ''
+      //const port = _.get(p, 'port') || ''     
+      //_.set(p, 'name', port + name +'name') 
+      _.set(p, 'ws-opts.headers.Host', 'p11.douyinpic.com') 
+      //_.set(p, 'xudp', true) // 开 xudp clash meta 核 vmess 支持 xudp
+      // _.set(p, 'udp', true) // 开 udp 一般不用在脚本里改 可以界面上开
+      // _.set(p, 'tfo', true) // 开 tfo 一般不用在脚本里改 可以界面上开
+    }
+    if(_.get(p, 'type') === 'trojan') {
+      //const name = _.get(p, 'name') || '' 
+      //const port = _.get(p, 'port') || ''
+      //_.set(p, 'name', port + name+'name')
+      _.set(p, 'skip-cert-verify', true)  // 改跳过证书验证
+      _.set(p, 'sni', 'p11.douyinpic.com') // 改混淆
+      // _.set(p, 'udp', true) // 开 udp 一般不用在脚本里改 可以界面上开
+      // _.set(p, 'tfo', true) // 开 tfo 一般不用在脚本里改 可以界面上开
+    }
+      return p
+  })
+}
