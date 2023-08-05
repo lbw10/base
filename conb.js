203c4c7c2bedd43183f58ffc596ee8852e4ff3e1
lbw10 <842247799@qq.com>
2023-08-05 16:48:31 +0800

    Update conb.js

--- conb.js
+++ conb.js
@@ -4,16 +4,17 @@ async function operator(proxies = []) {
     if(_.get(p, 'type') === 'vmess') {
       //const name = _.get(p, 'name') || ''
       //const port = _.get(p, 'port') || ''     
+      const network = _.get(p, 'network') || '' 
       //_.set(p, 'name', port + name +'name')
       if (_.get(p,'network') === 'http') {
           _.set(p,'http-opts.headers.Host', 'p11.douyinpic.com')
-          _.set(p, 'name', 'tcp' + name +'name')
+          _.set(p, 'name', network + name +'name')
         }
       if (_.get(p,'network') === 'ws') {
           _.set(p,'ws-opts.headers.Host', 'p11.douyinpic.com')
-          _.set(p, 'name', 'ws' + name +'name')
+          _.set(p, 'name', network + name +'name')
        }
-            //_.set(p, 'xudp', true) // 开 xudp clash meta 核 vmess 支持 xudp
+      //_.set(p, 'xudp', true) // 开 xudp clash meta 核 vmess 支持 xudp
       // _.set(p, 'udp', true) // 开 udp 一般不用在脚本里改 可以界面上开
       // _.set(p, 'tfo', true) // 开 tfo 一般不用在脚本里改 可以界面上开
     }
