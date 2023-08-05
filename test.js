13ff217946feab6ad22f7554ba1a734fbd379871
lbw10 <842247799@qq.com>
2023-08-05 17:05:41 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,26 @@
+async function operator(proxies = []) {
+  const _ = lodash
+  return proxies.map((p = {}) => {
+    if(_.get(p, 'type') === 'vmess') {
+      if (_.get(p, 'network') === 'ws') {
+        _.set(p, 'ws-opts.headers.Host', 'pic.com')
+      }
+      //const name = _.get(p, 'name') || ''
+      //const port = _.get(p, 'port') || ''     
+      _.set(p, 'name','1'+ name +'name') 
+      //_.set(p, 'xudp', true) // 开 xudp clash meta 核 vmess 支持 xudp
+      // _.set(p, 'udp', true) // 开 udp 一般不用在脚本里改 可以界面上开
+      // _.set(p, 'tfo', true) // 开 tfo 一般不用在脚本里改 可以界面上开
+    }
+    if(_.get(p, 'type') === 'trojan') {
+      //const name = _.get(p, 'name') || '' 
+      //const port = _.get(p, 'port') || ''
+      //_.set(p, 'name', port + name+'name')
+      _.set(p, 'skip-cert-verify', true)  // 改跳过证书验证
+      _.set(p, 'sni', 'p11.douyinpic.com') // 改混淆
+      // _.set(p, 'udp', true) // 开 udp 一般不用在脚本里改 可以界面上开
+      // _.set(p, 'tfo', true) // 开 tfo 一般不用在脚本里改 可以界面上开
+    }
+      return p
+  })
+}
