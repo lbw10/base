2a1a63c44ef1bd42abc8c8d66a270b5822ff56be
lbw10 <842247799@qq.com>
2023-09-02 19:21:11 +0800

    Update conb.js

--- conb.js
+++ conb.js
@@ -3,29 +3,23 @@ async function operator(proxies = []) {
   return proxies.map((p = {}) => {
     if(_.get(p, 'type') === 'vmess') {
       if (_.get(p, 'network') === 'ws') {
-        _.set(p, 'ws-opts.headers.Host', 'p11.douyinpic.com')
-        //_.set(p, 'name', 'ws' + name +'name')
+        if (_.get(p, 'tls') === true) {
+          _.set(p, 'ws-opts.headers.Host', 'api5-normal-c-lq.amemv.com')
+          _.set(p, 'sni', 'api5-normal-c-lq.amemv.com')
+        }
+        else {
+          _.set(p, 'ws-opts.headers.Host', 'api5-normal-c-lq.amemv.com')
+        }
       }
-      if (_.get(p, 'network') === 'http'){
-        _.set(p, 'http-opts.headers.Host', 'p11.douyinpic.com') 
+      if (_.get(p, 'network') === 'http') {
+        _.set(p, 'http-opts.headers.Host', 'api5-normal-c-lq.amemv.com') 
         _.set(p, 'name', 'tcp') 
       }
-      //const name = _.get(p, 'name') || ''
-      //const port = _.get(p, 'port') || ''     
-      //_.set(p, 'name', port + name +'name') 
-      //_.set(p, 'xudp', true) // 开 xudp clash meta 核 vmess 支持 xudp
-      // _.set(p, 'udp', true) // 开 udp 一般不用在脚本里改 可以界面上开
-      // _.set(p, 'tfo', true) // 开 tfo 一般不用在脚本里改 可以界面上开
     }
     if(_.get(p, 'type') === 'trojan') {
-      //const name = _.get(p, 'name') || '' 
-      //const port = _.get(p, 'port') || ''
-      //_.set(p, 'name', port + name+'name')
-      _.set(p, 'skip-cert-verify', true)  // 改跳过证书验证
-      _.set(p, 'sni', 'p11.douyinpic.com') // 改混淆
-      // _.set(p, 'udp', true) // 开 udp 一般不用在脚本里改 可以界面上开
-      // _.set(p, 'tfo', true) // 开 tfo 一般不用在脚本里改 可以界面上开
+      _.set(p, 'skip-cert-verify', true)
+      _.set(p, 'sni', 'api5-normal-c-lq.amemv.com')
     }
-      return p
+    return p
   })
 }
