e1961c58d1517cf37f81026d39282aa7c44a6b54
lbw10 <842247799@qq.com>
2023-09-15 12:50:03 +0800

    Update nota.js

--- loon plugin/nota.js
+++ loon plugin/nota.js
@@ -1,5 +1,5 @@
 [rewrite_local]
-^https?:\/\/notability\.com\/(global|subscriptions) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/notability.js
+^https?:\/\/notability\.com\/(global|subscriptions) url script-response-body https://raw.githubusercontent.com/lbw10/base/main/loon%20plugin/nota.js
 
 [mitm]
 hostname = notability.com
